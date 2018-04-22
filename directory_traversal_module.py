7bd2bc05f9245bac4f994f10e5335a3d4cbde099
garbanzos <yiyant@gmail.com>
2018-04-22 21:16:53 +0800

    add skeleton to try post and get req on endpts with payloads

--- directory_traversal_module.py
+++ directory_traversal_module.py
@@ -3,22 +3,52 @@ import requests
 from bs4 import BeautifulSoup
 
 def run(link, soup):
-  print("Running {}".format(link))
+  print("Running {}\n".format(link))
 
-  response = requests.get(link);
+  payloads = get_payload_list()
 
-  print(contains_passwd(response))
+  # TODO add checks
+  # if link is a get request
+  send_get_request(link, None, payloads)
 
-def get_request(link, soup):
+  # else if link is post request
+  # send_post_request(link, None, payloads)
 
+def get_payload_list(filename='directory_traversal_payloads.txt'):
+  """Returns payloads in payload file as a list"""
+  with open(filename) as f:
+    payloads = f.read().splitlines()
+    return payloads
 
-def post_request(link, soup):
+def send_get_request(link, soup, payloads):
+  """Try payloads in a get request"""
+  for payload in payloads:
+    # TODO for the time being, assumes that:
+    # 1. there is only one param
+    # 2. link given to it includes the param name
+    # e.g. http://target.com/directorytraversal/directorytraversal.php?ascii=
+
+    full_link = link + payload
+    print("Trying get request {}".format(full_link))
+    response = requests.get(full_link)
+    print("Exploitable with payload {}: {}\n".format(payload, contains_passwd(response)))
+
+def send_post_request(link, soup, payloads):
+  """Try payloads in a post request"""
+  for payload in payloads:
+    # TODO change post request
+    # 1. params and link?
+    # 2. headers?
+    # 3. need to send data as JSON?
+
+    print("Trying post request {}".format(link))
+    response = requests.post(link, data = {'key':payload})
+    print(response.text)
+    print("Exploitable with payload {}: {}\n".format(payload, contains_passwd(response)))
 
 def contains_passwd(response):
   """Checks if reponse constains /etc/passwd contents
-
-  Works by checking if the response contains
-  root:x:0:0:
+  Works by checking if the response contains 'root:x:0:0:'
 
   Args:
     response: Requests response object
@@ -26,7 +56,9 @@ def contains_passwd(response):
   Returns:
     boolean
   """
-  #TODO: need to find out if this is the best way to check /etc/passwd got displayed
+  # TODO: need to find out if this is the best way to check /etc/passwd got displayed\
+  #       maybe just change to ':x:0:0' because the username of root user may not always be root
+  #       but the uid and gid is guaranteed to be 0
 
   ROOT_LINE = 'root:x:0:0:'
 
@@ -38,7 +70,7 @@ def contains_passwd(response):
 
   # TODO: in case response contains JSON use reponses.json()
   # TODO: check if necessary - maybe using .text is sufficient
-  # tested with nusmods api - probably no need to use reponses.json() 
+  # tested with nusmods api - probably no need to use reponses.json()
 
   return False
 
