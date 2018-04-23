2c3d286d81f54a6da3809f4fdf1c91c31ba4a9cd
garbanzos <yiyant@gmail.com>
2018-04-23 20:14:56 +0800

    open redirect mod now returns result obj

--- open_redirect_module.py
+++ open_redirect_module.py
@@ -8,7 +8,7 @@ def run(injection_obj):
   print("Running {}\n".format(injection_obj[1]))
 
   payloads = get_payload_list()
-  inject_payloads(injection_obj, payloads)
+  return inject_payloads(injection_obj, payloads)
 
 def inject_payloads(injection_obj, payloads):
   method, link, params, cookie = injection_obj
@@ -20,15 +20,12 @@ def inject_payloads(injection_obj, payloads):
       print("Trying {} request {} with {} as param {}".format(method, link, payload, params[param_idx]))
       response = library.make_request(injection_obj, payload, param_idx)
       is_exploitable = is_successful_redirect(response)
-      print(response.text)
       print("Exploitable with payload {}: {}\n".format(payload, is_exploitable))
-      # TODO : write results into json
 
-      # TODO refactor this - break after finding the first successful payload
       if is_exploitable:
-          break
-    if is_exploitable:
-        break
+        return library.make_results_obj(injection_obj, payload)
+
+  return None
 
 def get_payload_list(filename='payloads/open_redirect_payloads.txt'):
   """Returns payloads in payload file as a list"""
@@ -64,4 +61,4 @@ def is_successful_redirect(response):
 # TODO for testing, remove when done
 if __name__ == '__main__':
   injection_obj = ['GET','http://127.0.0.1:8888/openredirect/openredirect.php', ['redirect'], '']
-  run(injection_obj)
+  print(run(injection_obj))
