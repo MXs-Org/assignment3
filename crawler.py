f689b67cfdcd95e67928bd7e39a73a9865bb3074
MX Chai <mingxuan@live.com.sg>
2018-04-23 19:33:08 +0800

    Update crawler to use urljoin

--- crawler.py
+++ crawler.py
@@ -4,15 +4,13 @@ Some lingo used in the files:
 - soup: a bs4.BeautifulSoup object, the result of BeautifulSoup(page.content, 'html.parser')
 """
 
+import urlparse
+
 import requests
 from bs4 import BeautifulSoup
 
 import scanner
-
-# Modify this
-# TODO: maybe change this to be a command line argument?
-# Note: DON'T end with a TRAILING '/'
-target_url = "http://localhost:8888"
+from library import BASE_URL, TARGET_URL
 
 def pass_to_scanner(link, soup):
 	# Final implementation is to pass URL to scanner once a link is found. 
@@ -20,21 +18,7 @@ def pass_to_scanner(link, soup):
 	scanner.scan(link, soup)
 
 def create_full_link(href, url):
-	# Checks if href is a relative or absolute URL. 
-	# Returns the fully qualified URL i.e. http://target.com/foo/bar
-	# TODO: what if there are weird things like "../"?
-	if href[0] == '/':
-		# Relative link
-		return url + href
-	elif href[0] == '.':
-		# Weird relative link, need to remove period
-		return url + href[1:]
-	elif not (href.startswith("http://") or href.startswith("https://")):
-		# Relative link
-		return url + "/" + href
-	else:
-		# href starts with the proper protocol i.e. http or https
-		return href
+	return urlparse.urljoin(url, href)
 
 def crawler(init_url):
 	# FILO queue
@@ -48,11 +32,14 @@ def crawler(init_url):
 		pass_to_scanner(url, soup)
 		# Find more links from the current URL
 		for a_element in soup.find_all('a', href=True):
-			link = create_full_link(a_element['href'], url)		
-			if link not in seen_links:
+			link = create_full_link(a_element['href'], url)
+			if BASE_URL not in link:
+				# Discard any links that are not in the scanning scope
+				continue	
+			if link and link not in seen_links:
 				seen_links.add(link)
 				if link not in all_links:
 					all_links.append(link)
 
 if __name__ == "__main__":
-	crawler(target_url)
+	crawler(TARGET_URL)
